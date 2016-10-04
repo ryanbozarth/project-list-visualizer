eff4f8e52561cec21a18e6d16e7933f2195b24d7
Ryan Bozarth <ryan.bozarth@gmail.com>
2016-10-03 18:57:54 -0700

    added tabletop.js

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 $(function() {
 
-  
+    var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/198drNvdXsVTAQ1QLTyRy8xlBmVhlrRpDaOAC2ZALzyI/pubhtml';
+
+    function init() {
+    Tabletop.init( { key: public_spreadsheet_url,
+                     callback: showInfo,
+                     simpleSheet: true } )
+  }
+
+  function showInfo(data, tabletop) {
+    var projectPhase = data.
+    console.log(data);
+  }
+
+  init();
 
 
-
-
-}); // end doc ready
+}) // end doc ready
