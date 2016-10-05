65ad119cc5c11c2630718190de80127275e23732
Ryan Bozarth <ryan.bozarth@gmail.com>
2016-10-04 19:47:16 -0700

    connected spreadsheet to webpage

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 $(function() {
 
-    var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/198drNvdXsVTAQ1QLTyRy8xlBmVhlrRpDaOAC2ZALzyI/pubhtml';
 
-    function init() {
-    Tabletop.init( { key: public_spreadsheet_url,
-                     callback: showInfo,
-                     simpleSheet: true } )
-  }
+        var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/198drNvdXsVTAQ1QLTyRy8xlBmVhlrRpDaOAC2ZALzyI/pubhtml';
 
-  function showInfo(data, tabletop) {
-    var projectPhase = data.
-    console.log(data);
-  }
+        function init() {
+            Tabletop.init({
+                key: public_spreadsheet_url,
+                callback: showInfo,
+                simpleSheet: true
+            })
+        }
 
-  init();
+        function showInfo(data, tabletop) {
+            for (var i = 0; i < data.length; i++) {
+                $('.project-status').append("<p class='line-item'>" + data[i].status + "</p>")
+                $('.project-name').append("<p class='line-item'>" + data[i].projectname + "</p>")
+                $('.project-launch').append("<p class='line-item'>" + data[i].launch + "</p>")
+                $('.project-team').append("<p class='line-item'>" + data[i].team + "</p>")
+                $('.project-remarks').append("<p class='line-item'>" + data[i].notes + "</p>")
+             }
+         }
+
+
+
+        init();
 
 
 }) // end doc ready
