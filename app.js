fafffcf3fe5d32ee45b42f77217cd12c8a67b2d9
Ryan Bozarth <ryan.bozarth@gmail.com>
2016-10-09 22:02:52 -0700

    added moments and finalized table

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 $(function() {
 
 
-        var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/198drNvdXsVTAQ1QLTyRy8xlBmVhlrRpDaOAC2ZALzyI/pubhtml';
+    var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/198drNvdXsVTAQ1QLTyRy8xlBmVhlrRpDaOAC2ZALzyI/pubhtml';
 
-        function init() {
-            Tabletop.init({
-                key: public_spreadsheet_url,
-                callback: showInfo,
-                simpleSheet: true
-            })
+    function init() {
+        Tabletop.init({
+            key: public_spreadsheet_url,
+            callback: showInfo,
+            simpleSheet: true
+        })
+    }
+
+    function showInfo(data, tabletop) {
+        for (var i = 0; i < data.length; i++) {
+            $('.project-status').after("<p class='line-item left status-color'>" + data[i].status + "</p>")
+            $('.project-team').after("<p class='line-item left'>" + data[i].team + "</p>")
+            $('.project-name').after("<p class='line-item left'>" + data[i].projectname + "</p>")
+            $('.project-launch').after("<p class='line-item center'>" + data[i].launch + "</p>")
+            $('.project-remarks').after("<p class='line-item left'>" + data[i].notes + "</p>")
+            console.log(data[i].status);
+        if (data[i].status == "Red") {
+            $('.status-color').css("background-color", "red");
+        } else if (data[i].status == "Yellow") {
+            $('.status-color').css("background-color", "yellow");
+        } else {
+            $('.status-color').css("background-color", "green");
         }
 
-        function showInfo(data, tabletop) {
-            for (var i = 0; i < data.length; i++) {
-                $('.project-status').append("<p class='line-item'>" + data[i].status + "</p>")
-                $('.project-name').append("<p class='line-item'>" + data[i].projectname + "</p>")
-                $('.project-launch').append("<p class='line-item'>" + data[i].launch + "</p>")
-                $('.project-team').append("<p class='line-item'>" + data[i].team + "</p>")
-                $('.project-remarks').append("<p class='line-item'>" + data[i].notes + "</p>")
-             }
-         }
+        }
+    };
+
+    setTimeout(function() {
+        location.reload();
+    }, 3000000);
 
 
 
-        init();
+    init();
 
 
-}) // end doc ready
+}); // end doc ready
